cfea3ff4fce1bf5055b103c6ca7d9aa506128c66
Yousaf KH <yousaf.k.hamza@gmail.com>
2024-10-17 17:56:51 +0530

    package tweak #2

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ SCRIPT_URL="https://raw.githubusercontent.com/yousafkhamza/header-checker/main/c
 # Installation directory
 INSTALL_DIR="$HOME/header-checker"
 SCRIPT_PATH="$INSTALL_DIR/check_headers.sh"
+LINK_PATH="/usr/local/bin/header-checker"
 
 # Check if the application is already installed
 if [ -f "$SCRIPT_PATH" ]; then
@@ -23,10 +24,11 @@ curl -sSL "$SCRIPT_URL" -o "$SCRIPT_PATH"
 # Make the script executable
 chmod +x "$SCRIPT_PATH"
 
-# Create a symlink in /usr/local/bin for easy access
-if ! command -v header-checker &> /dev/null; then
-  sudo ln -s "$SCRIPT_PATH" /usr/local/bin/header-checker
-  echo "Installation complete! You can now run the header checker using the command 'header-checker <url>'."
-else
+# Check if the symlink already exists
+if [ -L "$LINK_PATH" ]; then
   echo "The command 'header-checker' is already available in your PATH."
+else
+  # Create a symlink in /usr/local/bin for easy access
+  sudo ln -s "$SCRIPT_PATH" "$LINK_PATH"
+  echo "Installation complete! You can now run the header checker using the command 'header-checker <url>'."
 fi
